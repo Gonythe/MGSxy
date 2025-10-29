540d3895d3ebfb826c10362d252087af4880c97a
Gonythe <mael.kumm@gmail.com>
2025-10-29 16:04:39 +0100

    Add files via upload

--- jss/script_secret.js
+++ jss/script_secret.js
@@ -2,7 +2,12 @@ const button = document.getElementById("marietomaria");
 const maria = document.getElementById("maria");
 const marie = document.getElementById("marie");
 
+const audio = document.getElementById("silent-hill");
+
 button.addEventListener("click", () => {
   maria.classList.toggle("visible");
   marie.classList.toggle("invisible");
+
+  audio.muted = false;
+  audio.play();
 });
