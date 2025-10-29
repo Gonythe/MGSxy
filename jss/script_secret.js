9fbee6aa527663743ba3bf50b38ec3c81f0f1aca
Gonythe <mael.kumm@gmail.com>
2025-10-29 17:04:39 +0100

    Add files via upload

--- jss/script_secret.js
+++ jss/script_secret.js
@@ -2,7 +2,11 @@ const button = document.getElementById("marietomaria");
 const maria = document.getElementById("maria");
 const marie = document.getElementById("marie");
 
+const btn = document.getElementById("lettre");
+const overlay = document.getElementById("overlay");
+
 const audio = document.getElementById("silent-hill");
+const lettre_audio = document.getElementById("lettre-audio");
 
 button.addEventListener("click", () => {
   maria.classList.toggle("visible");
@@ -10,4 +14,17 @@ button.addEventListener("click", () => {
 
   audio.muted = false;
   audio.play();
+});
+
+btn.addEventListener("click", () => {
+  overlay.classList.toggle("visible");
+  marie.classList.toggle("invisible");
+  lettre_audio.currentTime = 0;
+
+  if (lettre_audio.paused) {
+    lettre_audio.play();       // lance l'audio
+    lettre_audio.muted = false; // s'assure que ce n'est pas en mute
+  } else {
+    lettre_audio.muted = !lettre_audio.muted; // toggle mute
+  }
 });
